fdbf2f66ee3c0e41d50ca114e1c53adf140b140f
yhy5847 <yhy5847@gmail.com>
2023-01-09 12:25:21 +0900

    Ch.18-3

--- test_c/소스.c
+++ test_c/소스.c
@@ -1,18 +1,8 @@
-#define _CRT_SECURE_NO_WARNINGS	
 #include <stdio.h>
 
 int main(void)
 {
-	int a, b;
-	int ans1, ans2, ans3, ans4;
-
-	scanf("%d%d", &a, &b);
-	ans1 = a * (b % 10);
-	ans2 = a * ((b % 100) / 10);
-	ans3 = a * (b / 100);
-	ans4 = a * b;
-	
-	printf("%d\n%d\n%d\n%d", ans1, ans2, ans3, ans4);
+	printf("         ,r\'\"7\nr`-_   ,\'  ,/\n \\. \". L_r\'\n   `~\\/\n      |\n      |");
 
 	return 0;
 }
